9d4c474357239fe1aadf718343d9ad58b29f0931
Gus <yo@soygus.com>
2018-03-02 19:05:40 -0600

    Removes firebase config from repo

--- src/environments/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCIewHt1kKx4tbZ9G67m6mfEbGTLoT0Uds',
-  authDomain: 'orocolo-wallet.firebaseapp.com',
-  databaseURL: 'https://orocolo-wallet.firebaseio.com',
-  projectId: 'orocolo-wallet',
-  storageBucket: '',
-  messagingSenderId: '944119237871'
-};
